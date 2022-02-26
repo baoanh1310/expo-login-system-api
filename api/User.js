c66515cf05c319819cd76218a3ac8f9212f707bc
baoanh1310 <dataholicguy@gmail.com>
2022-02-27 06:50:41 +0700

    define User db and connect to Mongodb

---
+++ api/User.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Signup
+router.post('/signup', (req, res) => {
+
+});
+
+// Login
+router.post('/login', (req, res) => {
+
+});
+
+module.exports = router;
