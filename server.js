1d08e494a349169dd423c3c610bd9b4a6f9caf87
baoanh1310 <dataholicguy@gmail.com>
2022-02-27 07:40:35 +0700

    commit before pushing to heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('./config/db');
+require('dotenv').config();
 
 const app = require('express')();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const UserRouter = require('./api/User');
 
